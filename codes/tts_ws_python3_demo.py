683f80232f38eb8566cf7ba7b785fb9fceae68ae
ALLinLLM <gradytvhooopuy@gmail.com>
2024-03-21 16:45:41 +0800

    feature: 增加换气，每当文本换行，可配置换气间隔时长 enhancement: 修改配置文件默认值, 防止新手配置出错 fix: pyyaml.load() need an arg Loader now. refactor: format with Black formatter

---
+++ codes/tts_ws_python3_demo.py
@@ -0,0 +1,220 @@
+# -*- coding:utf-8 -*-
+#
+#   author: iflytek
+#
+#  本demo测试时运行的环境为：Windows + Python3.7
+#  本demo测试成功运行时所安装的第三方库及其版本如下：
+#   cffi==1.12.3
+#   gevent==1.4.0
+#   greenlet==0.4.15
+#   pycparser==2.19
+#   six==1.12.0
+#   websocket==0.2.1
+#   websocket-client==0.56.0
+#   合成小语种需要传输小语种文本、使用小语种发音人vcn、tte=unicode以及修改文本编码方式
+#  错误码链接：https://www.xfyun.cn/document/error-code （code返回错误码时必看）
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+import websocket
+import datetime
+import hashlib
+import base64
+import hmac
+import json
+from urllib.parse import urlencode
+import time
+import ssl
+from wsgiref.handlers import format_date_time
+from datetime import datetime
+from time import mktime
+import _thread as thread
+import os
+import yaml
+
+
+STATUS_FIRST_FRAME = 0  # 第一帧的标识
+STATUS_CONTINUE_FRAME = 1  # 中间帧标识
+STATUS_LAST_FRAME = 2  # 最后一帧的标识
+
+
+class Ws_Param(object):
+    # 初始化
+    def __init__(self, APPID, APIKey, APISecret, Text, speed=50, vcn="x2_pengfei"):
+        self.APPID = APPID
+        self.APIKey = APIKey
+        self.APISecret = APISecret
+        self.Text = Text
+
+        # 公共参数(common)
+        self.CommonArgs = {"app_id": self.APPID}
+        # 业务参数(business)，更多个性化参数可在官网查看
+        # raw：未压缩的pcm
+        # lame：mp3 (当aue=lame时需传参sfl=1)
+        # speex-org-wb;7： 标准开源speex（for speex_wideband，即16k）数字代表指定压缩等级（默认等级为8）
+        # speex-org-nb;7： 标准开源speex（for speex_narrowband，即8k）数字代表指定压缩等级（默认等级为8）
+        # speex;7：压缩格式，压缩等级1~10，默认为7（8k讯飞定制speex）
+        # speex-wb;7：压缩格式，压缩等级1~10，默认为7（16k讯飞定制speex）
+        self.BusinessArgs = {
+            "aue": "lame",
+            "speed": speed,
+            "sfl": 1,
+            "auf": "audio/L16;rate=16000",
+            "vcn": vcn,
+            "tte": "utf8",
+        }
+        self.Data = {
+            "status": 2,
+            "text": str(base64.b64encode(self.Text.encode("utf-8")), "UTF8"),
+        }
+        # 使用小语种须使用以下方式，此处的unicode指的是 utf16小端的编码方式，即"UTF-16LE"”
+        # self.Data = {"status": 2, "text": str(base64.b64encode(self.Text.encode('utf-16')), "UTF8")}
+
+    # 生成url
+    def create_url(self):
+        url = "wss://tts-api.xfyun.cn/v2/tts"
+        # 生成RFC1123格式的时间戳
+        now = datetime.now()
+        date = format_date_time(mktime(now.timetuple()))
+
+        # 拼接字符串
+        signature_origin = "host: " + "ws-api.xfyun.cn" + "\n"
+        signature_origin += "date: " + date + "\n"
+        signature_origin += "GET " + "/v2/tts " + "HTTP/1.1"
+        # 进行hmac-sha256进行加密
+        signature_sha = hmac.new(
+            self.APISecret.encode("utf-8"),
+            signature_origin.encode("utf-8"),
+            digestmod=hashlib.sha256,
+        ).digest()
+        signature_sha = base64.b64encode(signature_sha).decode(encoding="utf-8")
+
+        authorization_origin = (
+            'api_key="%s", algorithm="%s", headers="%s", signature="%s"'
+            % (self.APIKey, "hmac-sha256", "host date request-line", signature_sha)
+        )
+        authorization = base64.b64encode(authorization_origin.encode("utf-8")).decode(
+            encoding="utf-8"
+        )
+        # 将请求的鉴权参数组合为字典
+        v = {"authorization": authorization, "date": date, "host": "ws-api.xfyun.cn"}
+        # 拼接鉴权参数，生成url
+        url = url + "?" + urlencode(v)
+        # print("date: ",date)
+        # print("v: ",v)
+        # 此处打印出建立连接时候的url,参考本demo的时候可取消上方打印的注释，比对相同参数时生成的url与自己代码生成的url是否一致
+        # print('websocket url :', url)
+        return url
+
+
+def on_message(ws, message):
+    try:
+        message = json.loads(message)
+        code = message["code"]
+        sid = message["sid"]
+        audio = message["data"]["audio"]
+        audio = base64.b64decode(audio)
+        status = message["data"]["status"]
+        print(message)
+        if status == 2:
+            print("ws is closed")
+            ws.close()
+        if code != 0:
+            errMsg = message["message"]
+            print("sid:%s call error:%s code is:%s" % (sid, errMsg, code))
+        else:
+
+            with open("../outputs/demo%04d.mp3" % id, "ab") as f:
+                f.write(audio)
+
+    except Exception as e:
+        print("receive msg,but parse exception:", e)
+
+
+# 收到websocket错误的处理
+def on_error(ws, error):
+    print("### error:", error)
+
+
+# 收到websocket关闭的处理
+def on_close(ws):
+    print("### closed ###")
+
+
+# 收到websocket连接建立的处理
+def on_open(ws):
+    def run(*args):
+        d = {
+            "common": wsParam.CommonArgs,
+            "business": wsParam.BusinessArgs,
+            "data": wsParam.Data,
+        }
+        d = json.dumps(d)
+        print("------> send data")
+        ws.send(d)
+        if os.path.exists("../outputs/demo%04d.mp3" % id):
+            os.remove("../outputs/demo%04d.mp3" % id)
+
+    thread.start_new_thread(run, ())
+
+
+if __name__ == "__main__":
+    # 读取配置文件
+    with open("../config.yaml", "r", encoding="utf-8") as yaml_f:
+        file_data = yaml_f.read()
+        config = yaml.load(file_data, Loader=yaml.FullLoader)
+    APPID = config["APPID"]
+    APIKey = config["APIKey"]
+    APISecret = config["APISecret"]
+    text_path = config["text_path"]
+    text_limit = config["text_limit"]
+    speed = config["speed"]
+    voice_name = config["voice_name"]
+    sentence_sep_iter = config["sentence_sep_iter"]
+
+    print("\n\nload config successfully\n\n")
+    # 读取文本
+    with open("../" + text_path, "r", encoding="UTF-8") as data:
+        lines = data.readlines()
+    id = 0
+    count = 0
+    text2000 = []
+    for line in lines:
+        line += "[p%d]" % sentence_sep_iter
+        count += len(line)
+        if count >= text_limit:
+            # 调用科大tts web API
+            wsParam = Ws_Param(
+                APPID=APPID,
+                APIKey=APIKey,
+                APISecret=APISecret,
+                Text="".join(text2000),
+                speed=speed,
+                vcn=voice_name,
+            )
+            websocket.enableTrace(False)
+            wsUrl = wsParam.create_url()
+            ws = websocket.WebSocketApp(
+                wsUrl, on_message=on_message, on_error=on_error, on_close=on_close
+            )
+            ws.on_open = on_open
+            ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+            text2000 = []
+            text2000.append(line)
+            count = len(line)
+            id += 1
+        else:
+            text2000.append(line)
+    wsParam = Ws_Param(
+        APPID=APPID,
+        APIKey=APIKey,
+        APISecret=APISecret,
+        Text="".join(text2000),
+        speed=speed,
+        vcn=voice_name,
+    )
+    websocket.enableTrace(False)
+    wsUrl = wsParam.create_url()
+    ws = websocket.WebSocketApp(
+        wsUrl, on_message=on_message, on_error=on_error, on_close=on_close
+    )
+    ws.on_open = on_open
+    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
---
+++ run.bat
@@ -0,0 +1,3 @@
+cd codes
+python tts_ws_python3_demo.py
+python combine.py
